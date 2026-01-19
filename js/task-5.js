cb8d167e1db9f9256294b141961cc8b53cf76866
komandor <koletttut@gmail.com>
2026-01-19 14:46:01 +0200

    add hm7

---
+++ js/task-5.js
@@ -0,0 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.body;
+
+
+btnChangeColor.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+});
